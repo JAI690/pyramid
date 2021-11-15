56639f2f96dddcab2adc18897b6cc86c88230cf7
Diana-Quinn <dianarenteria.virgo2000@gmail.com>
2021-11-14 19:39:09 -0600

    avance front

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,12 +8,15 @@ const router = express.Router();
 
 router.get('/', (req,res) => {
 
-res.render("../views/Info_productos/info_productos.hbs")
+res.render("../views/Home/home.hbs")
 });
 
-//../views/Contactanos/contactanos.hbs
+//../views/Contactanos/contactanos.hbs //le falta pop-ups
 //../views/auth/signin.hbs
-//../views/Productos/productos.hbs
-//../views/Info_productos/info_productos.hbs
+//../views/Productos/productos.hbs //no sirve, nomas era para ver las cards
+//../views/Info_productos/info_productos.hbs//no sirve, nomas tiene info xs
+//../views/Detalles_productos/detalles_productos.hbs
+//../views/Favoritos/favoritos.hbs
+//../views/Cuenta/cuenta.hbs
 
 module.exports = router;
